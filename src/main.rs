062390feba0240687479e272bb7182753cb8caac
octaltree <octaltree@gmail.com>
2017-06-16 22:02:00 +0900

    MySession, BlockUpdate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate legion;
+
+use legion::*;
+
+enum MySession {
+    EmptySession,
+}
+enum BlockUpdate {
+    UpdateData(Block),
+    ReplaceData(Vec<Block>),
+    RequestChain,
+}
+
 fn main() {
     println!("Hello, world!");
 }
