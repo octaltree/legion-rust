ff45d69fb1a1a787504947dd6a4566b153eceaeb
octaltree <octaltree@gmail.com>
2017-06-16 22:21:40 +0900

    add type

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,24 @@ use legion::*;
 enum MySession {
     EmptySession,
 }
+struct MainArgs {
+    http_port: String,
+    p2p_port: String,
+    seed_node: Option<String>,
+}
+struct BlockChainState {
+    state: Vec<Block>,
+    // TODO
+}
 enum BlockUpdate {
     UpdateData(Block),
     ReplaceData(Vec<Block>),
     RequestChain,
 }
+// TODO
+//instance B.Binary BlockUpdate
+
+const p2p_service_name: &str = "updateservice";
 
 fn main() {
     println!("Hello, world!");
